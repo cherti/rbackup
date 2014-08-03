4900ba56563b0bc0e6f2cc0e4eadf0e1d48fd56c
Jonas Große Sundrup <cherti@letopolis.de>
2014-08-03 02:15:16 +0200

    implemented a lot of logic of rbackup.py

--- rbackup.py
+++ rbackup.py
@@ -2,10 +2,9 @@
 
 conffile = 'sample.conf'
 
-import syncer, configparser, sys, os, checkconf
+import syncer, configparser, sys, os, checkconf, lockfile, time
 
-section = sys.argv[1]
-
+#
 #
 # preparations
 #
@@ -19,38 +18,146 @@ if conferrors:
 config = configparser.ConfigParser()
 config.read(conffile)
 
+lock = lockfile.Lockfile(config.get('general', 'lockfile'))
+
+# properly interpret cmdline-args
+if len(sys.argv) > 2:
+	section = sys.argv[1]
+	label = sys.argv[2]
+	if label not in config.options('labels'):
+		print('unknown label {0}'.format(label))
+		sys.exit(1)
+elif len(sys.argv) > 1:
+	section = sys.argv[1]
+	if section == 'main':
+		print('missing label for section main', file=sys.stderr)
+		sys.exit(1)
+else:
+	print('not enough information', file=sys.stderr)
+	sys.exit(1)
+
+
+# print mark
+
+if section == 'main' and label:
+	print('making backup to {0} on {1}'.format(label, section))
+elif section not in ['main', 'labels', 'general']:
+	print('duplicating backup to {0}'.format(section))
+
+
 # parse configuration
-#pre
-try: pre = config.get(section, 'pre')
-except configparser.NoOptionError: pre = ''
+def getconf(sec):
+	"""
+	parse configuration for given section and validate before returning
+	"""
 
-if not os.path.exists(pre): #fallback to nothing in case pre is invalid
-	pre = ''
+	#pre
+	try: pre = config.get(sec, 'pre')
+	except configparser.NoOptionError: pre = ''
 
-
-#post
-try: post = config.get(section, 'post')
-except configparser.NoOptionError: post = ''
-
-if not os.path.exists(post): #fallback to nothing in case post is invalid
-	post = ''
+	if not os.path.exists(pre):
+		print('pre-script for {0} is invalid, ignoring')
+		pre = ''
 
 
-#backupdir
-#parse
-try: backupdir = config.get(section, 'backupdir')
-except configparser.NoOptionError:
-	print('no backupdir specified', file=sys.stderr)
-	sys.exit(3)
+	#post
+	try: post = config.get(sec, 'post')
+	except configparser.NoOptionError: post = ''
 
-#check validity
-if not os.path.exists(backupdir):
-	print('invalid backupdir specified', file=sys.stderr)
-	sys.exit(4)
+	if not os.path.exists(post):
+		print('post-script for {0} is invalid, ignoring')
+		post = ''
+
+
+	#backupdir
+	#parse
+	try: path = config.get(sec, 'backupdir')
+	except configparser.NoOptionError:
+		print('no backupdir specified', file=sys.stderr)
+		sys.exit(3)
+
+
+	return pre, post, path
 
 
 #
 # everything fine, time to do some magic!
 #
 
+def prepare(prescript):
+	"""
+	prepare by using pre-script and check if everything seems ok
+	"""
 
+	preret = os.system(pre)
+
+	if preret != 0:
+		if preret == 32256:
+			print('no permission to execute pre-script; is it executable?', file=sys.stderr)
+
+		print('prescript {0} exited nonzero, exiting myself'.format(prescript), file=sys.stderr)
+		sys.exit(6)
+	else:
+		#check validity of mountpoint/backuppath
+		if not os.path.exists(path):
+			print('invalid backupdir', file=sys.stderr)
+			sys.exit(4)
+
+# lockfile
+# proper handling of returns
+
+while lock.isValid():
+	time.sleep(30)
+
+lock.create()
+
+if section == 'main': # system2backup
+	pre, post, dst = getconf('main')
+	src = config.get('general', 'backupsource')
+
+	prepare(pre)
+
+	rsync_ret = syncer.backup_sync(src, dst, label)
+
+	if rsync_ret != 0:
+		print('rsync finished with errors: exit code {0}'.format(rsync_ret), file=sys.stderr)
+
+	postret = os.system(post)
+
+	if postret != 0:
+		if preret == 32256:
+			print('no permission to execute pre-script; is it executable?', file=sys.stderr)
+
+		print('post-script finished with errors: exit code {0}'.format(postret), file=sys.stderr)
+
+elif not section in ['main', 'labels', 'general']:
+	mainpre, mainpost, src = getconf('main')
+	secpre, secpost, dst = getconf(section)
+
+	prepare(mainpre)
+	prepare(secpre)
+
+	rsync_ret = syncer.simple_sync(src, dst)
+
+	if rsync_ret != 0:
+		print('rsync finished with errors: exit code {0}'.format(rsync_ret), file=sys.stderr)
+
+
+	postret = os.system(mainpost)
+
+	if postret != 0:
+		if preret == 32256:
+			print('no permission to execute pre-script; is it executable?', file=sys.stderr)
+
+		print('post-script for section main finished with errors: exit code {0}'.format(postret), file=sys.stderr)
+
+	postret = os.system(secpost)
+
+	if postret != 0:
+		if preret == 32256:
+			print('no permission to execute pre-script; is it executable?', file=sys.stderr)
+
+		print('post-script for section {0} finished with errors: exit code {1}'.format(section, postret), file=sys.stderr)
+
+
+lock.remove()

7c32079bb8a82abcb50c6b147b1ef2d4cb58865a
Jonas Große Sundrup <cherti@letopolis.de>
2014-08-31 00:11:28 +0200

    minor changes + documentation

--- rbackup.py
+++ rbackup.py
@@ -4,46 +4,42 @@ conffile = 'sample.conf'
 
 import syncer, configparser, sys, os, checkconf, lockfile, time, argparse
 
-#
-# preparations
-#
 
-conferrors = checkconf.checkconfiguration(conffile)
+"""
+ preparations
+"""
 
+
+# check configuration validity
+checkconf.checkconfiguration(conffile)
+
+# parse configuration
 config = configparser.ConfigParser()
 config.read(conffile)
 
+# prepare lockfile
 lock = lockfile.Lockfile(config.get('general', 'lockfile'))
 
-#
-# handle commandline-args
-#
+
+# now parse commandline-args
 
 parser = argparse.ArgumentParser(description='some dummy description')
-
 # choose Backup or Duplicate
 parser.add_argument('-b', '--backup', action='store_true', default=False, dest='backup')
 parser.add_argument('-d', '--duplicate', action='store_true', default=False, dest='dupl')
 
-# specify whether the run shall be stored in case of disk not becoming available
-parser.add_argument('-s', action='store_true', default=False, dest='store')
-
-# specify targetdisk via section ('to')
-parser.add_argument('-t', '--to', action='store', default=None, dest='to')
-
-# specify sourcedisk for duplication ('from')
-parser.add_argument('-f', '--from', action='store', default=None, dest='fro')
-
-# specify label (to use in case of backup)
-parser.add_argument('-l', '--label', action='store', default=None, dest='label')
+parser.add_argument('-s', action='store_true', default=False, dest='store') # specify whether the run shall be stored in case of disk not becoming available
+parser.add_argument('-t', '--to', action='store', default=None, dest='to') # specify targetdisk via section ('to')
+parser.add_argument('-f', '--from', action='store', default=None, dest='fro') # specify sourcedisk for duplication ('from')
+parser.add_argument('-l', '--label', action='store', default=None, dest='label') # specify label (to use in case of backup)
 
 args = parser.parse_args(sys.argv[1:])
-
 
 # check whether args are such that we can work with them
 checkconf.checkargs(args, config)
 
 
+# giving status update what will be done (for user or logs)
 if args.backup:
 	print('making backup to label {0} on device {1}'.format(args.label, args.to))
 elif args.to not in ['labels', 'general']:
@@ -56,19 +52,21 @@ def getconf(sec):
 	parse configuration for given section and validate before returning
 	"""
 
-	#pre
-	try: pre = config.get(sec, 'pre')
-	except configparser.NoOptionError: pre = ''
+	#parsing pre
+	try: pre = config.get(sec, 'pre') # returns pre if pre exists in conf
+	except configparser.NoOptionError: pre = '' # else keep empty
 
+	# now validate if we can find the potential pre-script
 	if pre != '' and not os.path.exists(pre):
 		print('path to pre-script for {0} is invalid, ignoring'.format(sec))
 		pre = ''
 
 
-	#post
-	try: post = config.get(sec, 'post')
-	except configparser.NoOptionError: post = ''
+	#parsing post
+	try: post = config.get(sec, 'post') # returns post if post exists in conf
+	except configparser.NoOptionError: post = '' # else keep empty
 
+	# now validate if we can find the potential post-script
 	if post != '' and not os.path.exists(post):
 		print('path to post-script for {0} is invalid, ignoring'.format(sec))
 		post = ''
@@ -79,7 +77,7 @@ def getconf(sec):
 	try: path = config.get(sec, 'backupdir')
 	except configparser.NoOptionError:
 		print('no backupdir specified', file=sys.stderr)
-		sys.exit(3)
+		sys.exit(3) # exit, without backupdir there is no point in continuing
 
 
 	return pre, post, path
@@ -94,45 +92,49 @@ def prepare(prescript, path):
 	prepare by using pre-script and check if everything seems ok
 	"""
 
-	preret = os.system(prescript)
+	preret = os.system(prescript) # make specified preparations
 
 	if preret != 0:
-		if preret == 32256:
+		if preret == 32256: # no-permission-error
 			print('no permission to execute pre-script; is it executable?', file=sys.stderr)
 
-		print('prescript {0} exited nonzero, exiting myself'.format(prescript), file=sys.stderr)
-		sys.exit(6)
+		print('prescript {0} exited nonzero, exiting as well'.format(prescript), file=sys.stderr)
+		sys.exit(6) # pre exited nonzero, so we assume not to have device, no point in continuing
 	else:
 		#check validity of mountpoint/backuppath
 		if not os.path.exists(path):
 			print('invalid backupdir', file=sys.stderr)
-			sys.exit(4)
+			sys.exit(4) # without valid backupdir there is no point in continuing
 
 
-while lock.isValid(): # wait for other processes to terminate
+while lock.isValid(): # wait for other processes of this kind to terminate via block
 	time.sleep(30)
 
+# now create lock to let other processes know that we want to do a backup
 lock.create()
 
-if args.backup: # system2backup
-	pre, post, dst = getconf(args.to)
-	src = config.get('general', 'backupsource')
+if args.backup: # we want to create a backup
+	pre, post, dst = getconf(args.to) # get configuration
+	src = config.get('general', 'backupsource') # get source-directory
 
 	prepare(pre, dst)
 
-	# decide whether we want to sync a backup or to copy one
-	labels = config.options('labels')
-	curr_label_index = labels.index(args.label)
+	# decide whether we want to sync a entirely new backup or to copy one to a later stage
+	labels = config.options('labels') # get list of labels from config
+	curr_label_index = labels.index(args.label) # get the index of the current label in list
 
-	if curr_label_index == 0: # first label, sync from source
+
+	if curr_label_index == 0: # it's the first label, so sync a new backup
 		rsync_ret = syncer.backup_sync(src, dst, args.label)
 
 		if rsync_ret != 0:
 			print('rsync finished with errors: exit code {0}'.format(rsync_ret), file=sys.stderr)
-	else: # later label, sync from previous one
 
-		prev_label = labels[curr_label_index-1]
-		cp_ret = syncer.backup_copy(dst, prev_label, args.label)
+	else: # higher-order label, copy a backup into a later stage
+		prev_label = labels[curr_label_index-1] # get the previous label to copy from
+
+		cp_ret = syncer.backup_copy(dst, prev_label, args.label) # ACTION!!!
+
 		if cp_ret != 0:
 			print('cp finished with errors: exit code {0}'.format(cp_ret), file=sys.stderr)
 
@@ -141,40 +143,44 @@ if args.backup: # system2backup
 	postret = os.system(post)
 
 	if postret != 0:
-		if preret == 32256:
+		if preret == 32256: # no-permission-error
 			print('no permission to execute pre-script; is it executable?', file=sys.stderr)
 
 		print('post-script finished with errors: exit code {0}'.format(postret), file=sys.stderr)
 
-else: # duplicate backup, args.d
+else: # duplicate backup to another disk, args.dupl == True
+
+	# get configuration
 	srcpre, srcpost, src = getconf(args.fro)
 	dstpre, dstpost, dst = getconf(args.to)
 
 	prepare(srcpre, src)
 	prepare(dstpre, dst)
 
-	print()
 	rsync_ret = syncer.simple_sync(src, dst)
 
 	if rsync_ret != 0:
 		print('rsync finished with errors: exit code {0}'.format(rsync_ret), file=sys.stderr)
 
 
+	# tidy up afterwards
+
 	postret = os.system(srcpost)
 
 	if postret != 0:
-		if preret == 32256:
+		if preret == 32256: # no-permission-error
 			print('no permission to execute post-script of device {0}; is it executable?'.format(args.fro), file=sys.stderr)
 
 		print('post-script for device {0} finished with errors: exit code {1}'.format(args.fro, postret), file=sys.stderr)
 
+	# tidy up afterwards
 	postret = os.system(dstpost)
 
 	if postret != 0:
-		if preret == 32256:
+		if preret == 32256: # no-permission-error
 			print('no permission to execute post-script of device {0}; is it executable?'.format(args.to), file=sys.stderr)
 
 		print('post-script for device {0} finished with errors: exit code {1}'.format(args.to, postret), file=sys.stderr)
 
 
-lock.remove()
+lock.remove() # finally release lock when done
--- syncer.py
+++ syncer.py
@@ -10,7 +10,7 @@ config.read(conffile)
 
 def simple_sync(src, dst, add_args=None):
 	"""
-	sync two directories
+	simply sync two directories using the additional rsync-arguments provided
 	"""
 
 	rsync_cmd = ["rsync", "-a", "--delete", config.get('general', 'additional_rsync_args')]
@@ -35,25 +35,30 @@ def reorder_backupdirs(label, maxcount, directory=None):
 	"""
 	shifting all directories of the given label back by one
 	to make space for the new backup as label.0
-	includes mechanism to delete the last one if too many directorys
+	includes a mechanism to delete the last one if too many directorys
 	are present
+	also includes a mechanism to collect backups that seem to violate
+	the system to order the backups. Shouldn't have to be used, but
+	one never knows
 	"""
 	
-	olddir = None
+	olddir = None # used in case of chdir
 
 	if directory: # then switch to directory to ease latter code
-		# and save currentdir, of course
+		# and save currentdir, of course, to switch back later
 		olddir = os.path.abspath(os.path.curdir)
 		os.chdir(directory)
 
 
+	# get the directories in question to be reordered
 	dirs = sorted([ d for d in os.listdir() if d.startswith(label) ])
 
+	# handle "overgrowth"
 	if len(dirs) >= maxcount: # we need to delete the last one
-		shutil.rmtree(dirs[-1])
-		dirs.remove(dirs[-1])
+		shutil.rmtree(dirs[-1]) # actually remove the folder
+		dirs.remove(dirs[-1]) # remove the folder from dirlist
 
-	# now move everything by one
+	# now move everything back by one
 	for i in range(len(dirs)-1, -1, -1):
 		src = '{0}.{1}'.format(label, i)
 		dst = '{0}.{1}'.format(label, i+1)
@@ -62,6 +67,7 @@ def reorder_backupdirs(label, maxcount, directory=None):
 			if os.path.exists(dst):
 				# if we still have dst left, something is probably
 				# out of system. Remove those from the standard-tree
+				# save them into a separate folder with date appended
 				# and give notice for the user to check
 				os.mkdir('out-of-system')
 				now = datetime.datetime.today()
@@ -70,7 +76,7 @@ def reorder_backupdirs(label, maxcount, directory=None):
 
 			shutil.move(src, dst)
 
-	if olddir: # if we changed, change back now
+	if olddir: # if we changed directory, change back now
 		os.chdir(olddir)
 
 
@@ -78,32 +84,34 @@ def reorder_backupdirs(label, maxcount, directory=None):
 def backup_sync(source, backuppath, label):
 	"""
 	create a new backup snapshot from data into the backupdir
+	on lowest stage (higher stages are done by backup_copy()
 	"""
 
 	os.chdir(backuppath)
 
 	# filter for dirs with this label
-	dirs = [ d for d in os.listdir() if d.startswith(label) ]
+	dirs = sorted([d for d in os.listdir() if d.startswith(label)])
 
-	if len(dirs) == 0: # no backups yet, start from scratch
+	if len(dirs) == 0: # we have no backups yet, therefore start from scratch
 		os.mkdir("in_progress_{0}".format(label))
-	else: # backups present, go updating
-		os.system("cp -al {0}.0 in_progress_{0}".format(label))
+	else: # backups present, take one and update it (faster & saves space)
+		os.system("cp -al {0} in_progress_{1}".format(dirs[0], label))
 
 
 	# prepare excludes and includes as arguments for latter rsync-use
-	add_rsync_args = []
+	add_rsync_args = [] # to store the additional args like in- and excludes
 
+	# in- and excludelist are only used if they are specified
 	if 'includelist' in config.options('general'):
 		incl_lst = config.get('general', 'includelist')
 
-		if os.path.exists(incl_lst):
+		if os.path.exists(incl_lst): # use only if path is valid
 			add_rsync_args += ['--include-from=' + config.get('general', 'includelist')]
 
 	if 'excludelist' in config.options('general'):
 		excl_lst = config.get('general', 'excludelist')
 
-		if os.path.exists(excl_lst):
+		if os.path.exists(excl_lst): # use only if path is valid
 			add_rsync_args += ['--exclude-from=' + config.get('general', 'excludelist')]
 
 
@@ -116,7 +124,9 @@ def backup_sync(source, backuppath, label):
 		#reorder backups
 		backupcount = int(config.get('labels', label))
 
-		if os.path.exists(label + '.0'):
+		# if the one we want to use is free, we do not need to reorder
+		if os.path.exists(label + '.0'): 
+			# if label.0 is already in use, then we have to
 			reorder_backupdirs(label, backupcount)
 
 		#finally move the synced one to the start
@@ -129,25 +139,28 @@ def backup_sync(source, backuppath, label):
 
 def backup_copy(backuppath, srclabel, dstlabel):
 	"""
-	create a snapshot based on other latest snapshots
+	create a snapshot based on other latest snapshots for
+	higher stages 
 	"""
 
 	os.chdir(backuppath)
 
 	# filter for dirs with this label for src and dst
-	srcdirs = [ d for d in os.listdir() if d.startswith(srclabel) ]
-	dstdirs = [ d for d in os.listdir() if d.startswith(dstlabel) ]
+	srcdirs = sorted([ d for d in os.listdir() if d.startswith(srclabel) ])
+	dstdirs = sorted([ d for d in os.listdir() if d.startswith(dstlabel) ])
 
-	if len(srcdirs) == 0:
+	if len(srcdirs) == 0: # if we have no sourcen, we cannot do anything
 		print('previous stage not existent, aborting', file=sys.stderr)
 		sys.exit(1)
 
 	# get max number of dirs to store
 	dstmax = int(config.get('labels', dstlabel))
 
-	reorder_backupdirs(dstlabel, dstmax)
+	# reordering only necessary if label.0 is already in use
+	if os.path.exists('{0}.0'.format(dstlabel)):
+		reorder_backupdirs(dstlabel, dstmax) # free label.0
 
-	print("cp -alT {0} {1}.0".format(srcdirs[-1], dstlabel))
+	# create the new backup for this stage
 	cp_ret = os.system("cp -alT {0} {1}.0".format(srcdirs[-1], dstlabel))
 
 	return cp_ret

5e4aa9748e12cec96068362ce07554b4cc1d97e1
Jonas Große Sundrup <cherti@letopolis.de>
2015-01-04 23:55:39 +0100

    fixed version number and license

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='RBackup',
-    version='1.0',
+    version='0.7',
     author='Jonas Große Sundrup',
     author_email='cherti@letopolis.de',
     packages=['rbackup'],
     scripts=['bin/rbackup'],
     url='https://github.com/cherti/rbackup',
-    license='license.txt',
+    license='GPLv3',
     description='rsync-based backup-tool',
     long_description=open('readme.txt').read(),
 )
